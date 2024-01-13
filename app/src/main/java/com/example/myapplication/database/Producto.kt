69995f4c5389d217c6df4294380323dfed1830e1
Scrum Master <zanjavier9@gmail.com>
2024-01-13 00:08:21 -0500

    Chito Cambio Color Pantalla Principal

--- app/src/main/java/com/example/myapplication/database/Producto.kt
+++ app/src/main/java/com/example/myapplication/database/Producto.kt
@@ -7,7 +7,8 @@ data class Producto(
     val id: Long = -1,
     val nombre: String,
     val imagen: Bitmap?,  // Imagen como Bitmap
-    // Otros campos...
+    val precio: Double,
+    val descripcion: String,
     val fkProductoEmpresa: Long
 ) {
     companion object {
@@ -15,13 +16,15 @@ data class Producto(
         const val COLUMN_ID = "id_producto"
         const val COLUMN_NOMBRE = "nombre_producto"
         const val COLUMN_IMAGEN = "imagen_producto"
-        // Otros campos...
+        const val COLUMN_PRECIO = "precio_producto"
+        const val COLUMN_DESCRIPCION = "descripcion_producto"
         const val COLUMN_FK_PRODUCTO_EMPRESA = "fk_producto_empresa"
         const val CREATE_TABLE = "CREATE TABLE $TABLE_NAME " +
                 "($COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "$COLUMN_NOMBRE TEXT, " +
                 "$COLUMN_IMAGEN BLOB, " +  // Tipo BLOB para imágenes
-                // Otros campos...
+                "$COLUMN_PRECIO REAL, " +
+                "$COLUMN_DESCRIPCION TEXT, " +
                 "$COLUMN_FK_PRODUCTO_EMPRESA INTEGER, " +
                 "FOREIGN KEY($COLUMN_FK_PRODUCTO_EMPRESA) REFERENCES ${Empresa.TABLE_NAME}(${Empresa.COLUMN_ID}))"
     }
