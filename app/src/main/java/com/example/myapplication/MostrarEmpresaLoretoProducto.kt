5cf77ed8092c1d7b3996bd4cd3174c91f166418d
Scrum Master <zanjavier9@gmail.com>
2024-01-15 20:17:40 -0500

    Chito Incorporcacion de Pagina General Empresa Producto

--- app/src/main/java/com/example/myapplication/MostrarEmpresaLoretoProducto.kt
+++ app/src/main/java/com/example/myapplication/MostrarEmpresaLoretoProducto.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -8,7 +9,7 @@ import com.example.e_commerce.R
 import com.example.myapplication.database.DBHelperProducto
 import com.example.myapplication.database.Empresa
 
-class MostrarEmpresaLoretoProducto : AppCompatActivity() {
+class MostrarEmpresaLoretoProducto : AppCompatActivity(), EmpresaAdapter.ClickListener {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: EmpresaAdapter
@@ -18,15 +19,29 @@ class MostrarEmpresaLoretoProducto : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mostrar_empresa_loreto_producto)
 
-        recyclerView = findViewById(R.id.recyclerViewEmpresas)
+        recyclerView = findViewById(R.id.recyclerViewEmpresas) // Asegúrate de que este ID coincida con el de tu layout
         databaseHelper = DBHelperProducto(this)
 
         // Obtener y mostrar las empresas de Loreto
         val empresasLoretoP = databaseHelper.getEmpresasByCantonId(3)  // Cambia por el id_canton deseado
 
         // Configurar el RecyclerView
-        adapter = EmpresaAdapter(empresasLoretoP)
+        adapter = EmpresaAdapter(empresasLoretoP, this)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    //Redireccionamiento Boton Ver Empresa
+    override fun onVerEmpresaClick(position: Int) {
+        val empresaSeleccionada = adapter.empresas[position]
+
+        // Crear un Intent para la nueva actividad (DetalleEmpresaProducto)
+        val intent = Intent(this, DetalleEmpresaProducto::class.java)
+
+        // Puedes pasar el ID de la empresa a la nueva actividad
+        intent.putExtra("empresa_id", empresaSeleccionada.id)
+
+        // Iniciar la nueva actividad
+        startActivity(intent)
     }
 }
